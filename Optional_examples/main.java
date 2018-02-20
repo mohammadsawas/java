3eb0c5b3e6d58448ffc8e2f5f5963faa04760e3d
mohammmod <mohammmod@github.com>
2018-02-20 17:21:08 +0100

    this is just an optional in java

--- Optional_examples/main.java
+++ Optional_examples/main.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 public class main {
     public static void main(String[] args) {
-//just try//
+//just
         optional taker = new optional();
         Optional<TakePhoto> photo = taker.TakingSomePhotos();
         if (photo.isPresent()){
