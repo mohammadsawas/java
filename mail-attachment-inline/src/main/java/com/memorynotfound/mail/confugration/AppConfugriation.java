b3b21d5b8c74a5a036a716cf50e474b2b650eb2f
mohammmod <mohammad.sawas2@outlook.com>
2018-05-04 17:10:31 +0200

    add sending emails

---
+++ mail-attachment-inline/src/main/java/com/memorynotfound/mail/confugration/AppConfugriation.java
@@ -0,0 +1,34 @@
+package com.memorynotfound.mail.confugration;
+
+import com.memorynotfound.mail.Application;
+import com.memorynotfound.mail.service.EmailService;
+import com.memorynotfound.mail.model.Mail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfugriation {
+    private static Logger log = LoggerFactory.getLogger(Application.class);
+
+    @Autowired
+    private EmailService emailService;
+
+    @Bean
+    ApplicationRunner applicationRunner() throws Exception{
+        return applicationArguments -> {
+            log.info("Spring Mail - Sending Email with Inline Attachment Example");
+
+            Mail mail = new Mail();
+            mail.setFrom("no-reply@memorynotfound.com");
+            mail.setTo("@gmail.com");
+            mail.setSubject("Sending Email with Inline Attachment Example");
+            mail.setContent("welcome bro");
+
+            emailService.sendSimpleMessage(mail);
+        };
+    }
+}
---
+++ mail-attachment-inline/src/main/java/com/memorynotfound/mail/service/EmailService.java
@@ -0,0 +1,33 @@
+package com.memorynotfound.mail.service;
+
+import com.memorynotfound.mail.model.Mail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.nio.charset.StandardCharsets;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public void sendSimpleMessage(Mail mail) throws MessagingException {
+
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,
+                MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                StandardCharsets.UTF_8.name());
+
+        helper.setText( mail.getContent(), true);
+        helper.setSubject(mail.getSubject());
+        helper.setTo(mail.getTo());
+        helper.setFrom(mail.getFrom());
+        emailSender.send(message);
+    }
+
+}
